312ba35c6fbb5227a28090f487c50c723a6a14a3
Lenka Galcikova <lenka.galcikova@stuba.sk>
2023-03-20 16:33:58 +0100

    paper:results started

--- data/plot_results.m
+++ data/plot_results.m
@@ -4,26 +4,32 @@ close all
 
 fs = 12;
 lw = 1.5;
+red = [204 0 0]/255;
+green = [50,205,50]/255;
+blue = [[0 0.4470 0.7410]];
 set(0,'defaulttextinterpreter','latex')
 
-figure
-plot(out.u(:,1),out.u(:,2),'LineWidth', lw)
-set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
+figure, hold on
+plot(out.u(:,1),out.u(:,2),'Color', blue,'LineWidth', lw)
+plot([0 25],[0 0],'r--','Color', red,'LineWidth', lw)
+plot([0 25],[100 100],'r--','Color', red,'LineWidth', lw)
 set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
 xlabel('time [s]')
 ylabel('fan speed [\%]')
 ylim([-10 110])
-% legend(..., 'Interpreter','latex', 'FontSize', fs)
+legend('$u(t)$','$u_\mathrm{min}, u_\mathrm{max}$', 'Interpreter','latex', 'FontSize', fs)
 
 figure, hold on
-plot(out.y(:,1),out.y(:,2),'LineWidth', lw)
-plot(out.y(:,1),out.y(:,3),'LineWidth', lw)
-set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
+plot(out.y(:,1),out.y(:,3),'Color', green,'LineWidth', lw)
+plot(out.y(:,1),out.y(:,2),'Color', blue,'LineWidth', lw)
+
+plot([0 25],[0 0],'--','Color', red, 'LineWidth', lw)
+plot([0 25],[100 100],'--','Color', red,'LineWidth', lw)
 set(gca, 'TickLabelInterpreter','latex','FontSize', fs)
 xlabel('time [s]')
 ylabel('flex sensor bend [\%]')
-ylim([0 100])
-legend('$y_\mathrm{ref}$', '$y(t)$', 'Interpreter','latex', 'FontSize', fs)
+ylim([-10 110])
+legend('$y^\mathrm{s}$', '$y(t)$', '$y_\mathrm{min}, y_\mathrm{max}$', 'Interpreter','latex', 'FontSize', fs)
 
 %tube3 - small offset
 %tube4 - nice but -68% not feasible
